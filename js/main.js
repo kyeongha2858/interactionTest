e3cba309e5aadadab1752af5832aaf838d555bdf
kyeongha2858 <kha2858@naver.com>
2020-09-05 21:00:43 +0900

    section4 clear

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log("main.js");
+(() => {
+    const leaflet = document.querySelector(".leaflet");
+
+    function getTarget(elem, className){ //부모노드까지 위임
+        while(!elem.classList.contains(className)){
+            elem = elem.parentNode;
+
+            if(elem.nodename =='BODY'){
+                elem = null;
+                return;
+            }
+        }
+        return elem;
+    }
+
+    leaflet.addEventListener('click',e =>{
+        let pageElem = getTarget(e.target, 'page');
+        if(pageElem){
+            pageElem.classList.add('page-flipped');
+        }
+    });
+})();
